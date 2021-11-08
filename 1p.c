31d30f9ac97427704bb04d04c188c10aed6058f2
dima-tolmachev <dima.tolmachev@gmail.com>
2021-11-08 23:43:41 +0400

    1st problem

--- 1p.c
+++ 1p.c
@@ -1,19 +1,9 @@
 #include <stdio.h>
 
-int spacesRemain(int n) {
-    int spaces = 3;
-    if (n > 9) { spaces = 2; }
-    if (n > 99) { spaces = 1; }
-    return(spaces);    
-}
-
 int main() {
-    for (int m = 1; m <= 12; m++) {
-        for (int i = 1; i <= 12; i++) {
-            for (int j = 0; j < spacesRemain(i * m); j++) {
-                printf(" ");
-            }
-            printf("%d", i * m);
+    for (int i = 1; i <= 12; i++) {
+        for (int j = 1; j <= 12; j++) {
+            printf("%4d", i * j);
         }
         printf("\n");
     }    
--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 I don't have a clue how I found this [resource](https://www.lix.polytechnique.fr/~liberti/public/computing/prog/c/C/PROBLEMS/problems.html). Anyway, those problems seem quite interesting. I've used **С** only a couple of times in my life in developing nonsense for Arduino.
 
 
-**0/14**
+**1/14**
